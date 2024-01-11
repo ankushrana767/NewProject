6f7a0ca5940cf3cb44979127b2e2325ef8d43860
ankush <ankushrana767@gmail.com>
2024-01-10 22:54:32 -0500

    Initial Commit

---
+++ NewProject/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  NewProject
+//
+//  Created by user238140 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("Campus")
+            Text("Ankush Rana")
+                .bold()
+            Text("Contact-  +1(365)8835722")
+            Text("Gmail - ankushrana767@gmail.com")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewProject/NewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewProjectApp.swift
+//  NewProject
+//
+//  Created by user238140 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
